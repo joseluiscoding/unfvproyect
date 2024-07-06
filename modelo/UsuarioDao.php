65426d219a32e8cf64cb9034b875603e58b6a782
De-la-Cruz-Jose <2022000812@unfv.edu.pe>
2024-07-06 02:22:41 -0500

    base  de datos cambiada

--- modelo/UsuarioDao.php
+++ modelo/UsuarioDao.php
@@ -6,7 +6,7 @@ class UsuarioDao{
     public function estaRegistradoUsuario(UsuarioBean $usuobj){
         $objc = new ConexionBD();
         $con=$objc->getConexionBD();
-        $sql = "SELECT * FROM usuario WHERE BINARY correo='" .$usuobj->getCorreo()."' AND contraseña='" . $usuobj->getContraseña()."'";     //jala los valores de usuarioBean
+        $sql = "SELECT * FROM login WHERE BINARY correo='" .$usuobj->getCorreo()."' AND contraseña='" . $usuobj->getContraseña()."'";     //jala los valores de usuarioBean
         $rs = mysqli_query($con,$sql);                                                                                                      //realiza la consulta
         if(mysqli_num_rows($rs) > 0) {                                                                                                      //busca en la tabla si coincide un valor
             return true; 
@@ -17,7 +17,7 @@ class UsuarioDao{
     public function devolverTipoUsuario(UsuarioBean $usuobj){
         $objc = new ConexionBD();
         $con=$objc->getConexionBD();
-        $sql = "SELECT * FROM usuario WHERE BINARY correo='" .$usuobj->getCorreo()."' AND contraseña='" . $usuobj->getContraseña()."'";
+        $sql = "SELECT * FROM login WHERE BINARY correo='" .$usuobj->getCorreo()."' AND contraseña='" . $usuobj->getContraseña()."'";
     }
 }
 
