de0326acca3ccabb62ba3387ae93625c171c6308
De-la-Cruz-Jose <2022000812@unfv.edu.pe>
2024-07-27 00:53:21 -0500

    cambios admin bean y dao

--- modelo/AdminDao.php
+++ modelo/AdminDao.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'AdminBean.php';                                             //importo la direccion para usar metodos
+require_once 'AdminBean.php';                                               //importo la direccion para usar metodos
 require_once '../util/Conexion_BD.php';                                     //importo la direccion para usar metodos para la conexion a la base de datos
 
 class AdminDao{
@@ -8,6 +8,20 @@ class AdminDao{
     public function guardarDocentes(AdminBean $objAdmin){
         try{
             $sql = "INSERT INTO docentes(nombresDocente, apellidosDocente, correoDocente) VALUES('$objAdmin->Nombre','$objAdmin->Apellido','$objAdmin->Correo')";
+            
+            $objc=new ConexionBD();
+            $cn=$objc->getConexionBD();
+            $rs=mysqli_query($cn,$sql);
+            mysqli_close($cn);
+        }catch (Exception $e){
+
+        }
+        return $rs;
+    }
+
+    public function guardarCursos(AdminBean $objAdmin){
+        try{
+            $sql = "INSERT INTO cursos(codCurso, nombreCurso) VALUES('$objAdmin->CodCurso','$objAdmin->NombreCurso')";
             
             $objc=new ConexionBD();
             $cn=$objc->getConexionBD();
