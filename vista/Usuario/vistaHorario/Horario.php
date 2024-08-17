47a6c10bb3ede930da3aec51916216d5b0a951b3
De-la-Cruz-Jose <2022000812@unfv.edu.pe>
2024-08-16 22:28:23 -0500

    estilo de cuadros cambios

--- vista/Usuario/vistaHorario/Horario.php
+++ vista/Usuario/vistaHorario/Horario.php
@@ -17,10 +17,25 @@
 
     <script> 
         $(document).ready(function(){
-            let table = new DataTable('#TablaHorarios');
-
-        }
-        );
+            let table = new DataTable('#TablaHorarios', {
+                "language": {
+                    "lengthMenu": "Mostrar _MENU_ entradas",
+                    "zeroRecords": "No se encontraron resultados",
+                    "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+                    "infoEmpty": "Mostrando 0 a 0 de 0 entradas",
+                    "infoFiltered": "(filtrado de _MAX_ entradas totales)",
+                    "search": "Buscar:",
+                    "paginate": {
+                        "first": "Primero",
+                        "last": "Último",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
+                    "loadingRecords": "Cargando...",
+                    "processing": "Procesando..."
+                }
+            });
+        });
     </script>
 </head>
 
